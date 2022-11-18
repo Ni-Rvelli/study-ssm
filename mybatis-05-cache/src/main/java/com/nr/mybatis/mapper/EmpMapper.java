74556391dc933ae7b23d7ddcff71fe5f316e060e
nirui <626340704@qq.com>
2022-11-18 17:43:38 +0800

    mybatis缓存

--- mybatis-05-cache/src/main/java/com/nr/mybatis/mapper/EmpMapper.java
+++ mybatis-05-cache/src/main/java/com/nr/mybatis/mapper/EmpMapper.java
@@ -12,5 +12,5 @@ import java.util.List;
  */
 public interface EmpMapper {
 
-
+    Emp getByEmpId(@Param("empId") Integer empId);
 }
--- mybatis-05-cache/src/test/java/MyBatisTest.java
+++ mybatis-05-cache/src/test/java/MyBatisTest.java
@@ -1,4 +1,5 @@
 import com.nr.mybatis.mapper.EmpMapper;
+import com.nr.mybatis.pojo.Emp;
 import com.nr.mybatis.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -12,11 +13,15 @@ public class MyBatisTest {
 
 
     @Test
-    public void testGetEmpByCondition() {
+    public void testCacheOne() {
         SqlSession sqlSession = SqlSessionUtil.getSqlSession();
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
-
-//        System.out.println(empList);
+        Emp emp = mapper.getByEmpId(1);
+        System.out.println(emp);
+        SqlSession sqlSession1 = SqlSessionUtil.getSqlSession();
+        EmpMapper mapper1 = sqlSession1.getMapper(EmpMapper.class);
+        Emp emp1 = mapper1.getByEmpId(1);
+        System.out.println(emp1);
         sqlSession.close();
     }
 
